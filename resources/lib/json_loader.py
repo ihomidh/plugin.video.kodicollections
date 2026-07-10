a222b06dc7eef88c783d88e0f4eedb7657768bc9
ihomidh <ihomidhi@gmail.com>
2026-07-10 15:56:50 +0300

    Update json_loader.py

--- resources/lib/json_loader.py
+++ resources/lib/json_loader.py
@@ -1,1 +1,25 @@
+import json
+import os
+import xbmcvfs
 
+
+class JsonLoader:
+    def __init__(self, addon_path):
+        self.addon_path = addon_path
+
+    def load(self, filename="collections.json"):
+        path = os.path.join(
+            self.addon_path,
+            "resources",
+            "data",
+            filename
+        )
+
+        if not xbmcvfs.exists(path):
+            return {}
+
+        file = xbmcvfs.File(path)
+        data = file.read()
+        file.close()
+
+        return json.loads(data)
