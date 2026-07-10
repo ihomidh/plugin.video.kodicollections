ed6e1f34a55bab85128bc64782fa4d82e80e7830
ihomidh <ihomidhi@gmail.com>
2026-07-10 16:09:29 +0300

    Update router.py

--- resources/lib/router.py
+++ resources/lib/router.py
@@ -1,21 +1,34 @@
-import xbmcaddon
+import sys
+from urllib.parse import parse_qsl
+
 import xbmcgui
 import xbmcplugin
+import xbmcaddon
 
 from resources.lib.json_loader import JsonLoader
 
 
 class Router:
 
-    def run(self, argv):
-
-        handle = int(argv[1])
+    def __init__(self):
+        self.handle = int(sys.argv[1])
+        self.params = dict(parse_qsl(sys.argv[2][1:]))
 
         addon = xbmcaddon.Addon()
+        self.addon_path = addon.getAddonInfo("path")
 
-        addon_path = addon.getAddonInfo("path")
+    def run(self, argv):
 
-        loader = JsonLoader(addon_path)
+        action = self.params.get("action")
+
+        if action == "open":
+            self.show_collection()
+        else:
+            self.show_home()
+
+    def show_home(self):
+
+        loader = JsonLoader(self.addon_path)
 
         data = loader.load()
 
@@ -23,13 +36,35 @@ class Router:
 
         for menu in menus:
 
+            url = (
+                f"{sys.argv[0]}?"
+                f"action=open&id={menu['id']}"
+            )
+
             item = xbmcgui.ListItem(label=menu["title"])
 
             xbmcplugin.addDirectoryItem(
-                handle,
-                "",
+                self.handle,
+                url,
                 item,
                 True
             )
 
-        xbmcplugin.endOfDirectory(handle)
+        xbmcplugin.endOfDirectory(self.handle)
+
+    def show_collection(self):
+
+        collection = self.params.get("id")
+
+        item = xbmcgui.ListItem(
+            label=f"Selected: {collection}"
+        )
+
+        xbmcplugin.addDirectoryItem(
+            self.handle,
+            "",
+            item,
+            False
+        )
+
+        xbmcplugin.endOfDirectory(self.handle)
