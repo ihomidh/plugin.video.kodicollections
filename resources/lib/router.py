49e3d7b42f40445ba42b00a0ee39cb2563efa917
ihomidh <ihomidhi@gmail.com>
2026-07-10 15:43:29 +0300

    Update router.py

--- resources/lib/router.py
+++ resources/lib/router.py
@@ -1,1 +1,31 @@
+import xbmcgui
+import xbmcplugin
 
+
+class Router:
+
+    def run(self, argv):
+
+        handle = int(argv[1])
+
+        menus = [
+            "🔥 Trending",
+            "⭐ Popular",
+            "🆕 New Releases",
+            "📺 Streaming Services",
+            "🎬 Collections",
+            "🎭 Genres",
+            "❤️ Trakt",
+            "⚙ Settings"
+        ]
+
+        for menu in menus:
+            item = xbmcgui.ListItem(label=menu)
+            xbmcplugin.addDirectoryItem(
+                handle,
+                "",
+                item,
+                True
+            )
+
+        xbmcplugin.endOfDirectory(handle)
